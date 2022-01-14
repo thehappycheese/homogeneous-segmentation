5d02182b5ae5797212917ae7a16d6f45dc96b50a
MrNickArcher <nicholas.archer@mainroads.wa.gov.au>
2022-01-14 16:54:29 +0800

    port of shs completed. to be tested.

--- src/HS/util/shs.py
+++ src/HS/util/shs.py
@@ -11,19 +11,14 @@ def _cumq (data:npt.ArrayLike) -> npt.ArrayLike: # debug: use cumq to calculate 
     # NOTE: In the original cum_n is used to index data, resulting in a reduction in series length by 1.
     #       I am not sure if this is an error?
     #       note that to preserve the original functionality data_left and data_right have been selected to drop one element from the data also
+    #       see _cumq_fixed below
    
     cum_n                = np.arange(1, len(data))  # 1:(n - 1)         # Used as denominator later ∴ Must start from 1.
     assert len(cum_n) == len(data) -1               # this preserves original functionality though i think it is possibly an error?
     #data_left          <- data[cum_n]
     data_left            = data[:-1]                # or data[0:n - 1]   # Drops last
     #data_right         <- rev(data)[cum_n]
-    data_right           = data[::-1][:-1]          # or data[-2::-1]    # Drops first and reverses
-
-    # The following corrects the error:
-    #cum_n                = np.arange(len(data)) + 1
-    #assert len(cum_n) == len(data) 
-    #data_left            = data
-    #data_right           = data[::-1]
+    data_right           = data[::-1][:-1]          # or     # reverses and drops what was the first item of original
     
     cum_data_left        = np.cumsum(data_left)
     cum_data_right       = np.cumsum(data_right)[::-1]
@@ -41,7 +36,7 @@ def _cumq (data:npt.ArrayLike) -> npt.ArrayLike: # debug: use cumq to calculate 
     )
 
 
-def _seg2 (data:pandas.DataFrame, var:list[str], length:str, allowed_segment_length_range:tuple[float, float]):
+def _seg2 (data:pandas.DataFrame, var:list[str], length:str, allowed_segment_length_range:tuple[float, float]) -> list[int]:
     
     min_length, _max_length = allowed_segment_length_range
 
@@ -50,19 +45,15 @@ def _seg2 (data:pandas.DataFrame, var:list[str], length:str, allowed_segment_len
 
     cumlength_left  = np.cumsum(data[length].to_numpy())
     cumlength_right = np.cumsum(data[length].to_numpy()[::-1])[::-1]
-
-    # all of this can be skipped
-    ## k_left        <- c(1, which(cumlength_left <= min_length) + 1)
-    #k_left          =                   np.append(np.array([0]), np.flatnonzero((cumlength_left <= min_length).to_numpy()) + 1)
-    ## k_right       <- nrow(data_var) - c(0, which(cumlength_right <= min_length))
-    #k_right         = len(data.index) - np.append(np.array([1]), np.flatnonzero((cumlength_right <= min_length).to_numpy()))
-    ## k             = c(1:nrow(data.var))[-c(k_left, k_right)]
-    #k               = np.delete(np.arange(0, len(data.index), np.append(k_left, k_right)))
     
     k_mask = ~ (
-          (cumlength_left <=min_length)
-        | (cumlength_right<=min_length)
+          (cumlength_left  <= min_length)
+        | (cumlength_right <= min_length)
     )
+
+    # to match the behavior of the R script we must
+    # expand the false values in the array by one index either side:
+    k_mask = ~np.convolve(~k_mask, np.array([True, True, True]))[1:-1]
 
     k = np.flatnonzero(k_mask)
 
@@ -84,15 +75,15 @@ def _seg2 (data:pandas.DataFrame, var:list[str], length:str, allowed_segment_len
             # fixing the error:
             #_cumq(data.loc[:,each_var].to_numpy())[k_mask]
         )
-    qvalue     = np.concatenate(qvalue_columns, axis=1)
+    
     # qvalue = rowMeans(qvalue)
-    qvalue_col = np.mean(qvalue, axis=1)
+    qvalue     = np.mean(np.array(qvalue_columns), axis=0)
     
     # This next line appears to retrieve the index of the global maximum mean q value.
     # however there is a danger that it could return a list instead of a scalar
     # # maxk <- which(qvalue == max(qvalue)) + max(k_left)
-    # maxk = np.flatnonzero(qvalue_col == qvalue_col.max()) + k_left.max()
-    maxk = np.argmax(qvalue_col) + min(0, k[0]-1)
+    maxk = np.flatnonzero(qvalue == np.max(qvalue)) + k[0]
+    # maxk = np.argmax(qvalue) + k[0]
     return maxk
 
 
@@ -121,10 +112,6 @@ def _cumq_fixed (data:npt.ArrayLike) -> npt.ArrayLike: # debug: use cumq to calc
     )
 
 
-
-
-
-
 def _seg2_fixed (data:pandas.DataFrame, var:list[str], length:str, allowed_segment_length_range:tuple[float, float]):
     
     min_length, _max_length = allowed_segment_length_range
@@ -139,6 +126,10 @@ def _seg2_fixed (data:pandas.DataFrame, var:list[str], length:str, allowed_segme
           (cumlength_left <=min_length)
         | (cumlength_right<=min_length)
     )
+
+    # to match the behavior of the R script we must
+    # expand the false values in the array by one index either side:
+    k_mask = ~np.convolve(~k_mask, np.array([True, True, True]))[1:-1]
 
     k = np.flatnonzero(k_mask)
 
@@ -160,6 +151,7 @@ def _seg2_fixed (data:pandas.DataFrame, var:list[str], length:str, allowed_segme
     
     maxk = np.argmax(qvalue_col) + min(0, k[0]-1)
     return maxk
+
 
 """
 Spatial heterogeneity-based segmentation (SHS) for homogeneous segmentation of spatial lines data.
@@ -185,43 +177,52 @@ def shs (data:pandas.DataFrame, var:list[str], length:str, allowed_segment_lengt
 
     
     # first segmentation
-    ss          = _seg2_original(data, var, length, allowed_segment_length_range)
+    ss          = _seg2(data, var, length, allowed_segment_length_range)
+    #ss = np.array([3,5])
     # following segmentation
-    k1          = pandas.Series([1, ss])
-    k2          = pandas.Series([ss - 1, len(data.index)])
-    ll          = k2 - k1 + 1
-    cum_ll      = (0, *ll.cumsum())
-    segid       = list(range(0, len(ll))) * ll
-    segdatalist = np.split(data, segid)
+    k1          = np.array([0, *ss])
+    k2          = np.array([*ss, len(data.index)])
+    ll          = k2 - k1
+    cum_ll      = np.append(np.array([0]), np.cumsum(ll))
+    segid       = np.repeat(np.arange(0, len(ll)), ll)
+    #segdatalist = np.split(data, segid)
 
-    #lengthdata        = pandas.DataFrame([data.loc[:,length], seg.id], columns = ["length","seg.id"]))
-    #seglength_summary = lengthdata[, .(sum(length)), by = .(seg.id)]
-    #seglength         = round(seglength_summary[[2]], digits = 10)
-    #k = which(seglength > max_length)
+    # we are grouping the data wherever _seg2 found a maximum. generally there should be only a single maximum, but if there is an equal maximum at two indices, then there will be 3 groups overall.
+    segdatalist = np.split(data, np.cumsum(ll)[:-1])
 
-    # while(len(k) > 0){
-    #     sa = sapply(k, function(x){
-    #         seg2(segdatalist[[x]], min_length) + cum_ll[x]
-    #     })
-    #     ss = sort(c(ss, sa))
+    #lengthdata        = pandas.DataFrame([data.loc[:,length], segid], columns = ["length","seg.id"]))
+    seglength_summary = data[length].groupby(segid).sum()
 
-    #     k1 = c(1, ss)
-    #     k2 = c(ss - 1, nrow(data))
-    #     ll = k2 - k1 + 1
-    #     cum_ll = c(0, cumsum(ll))
-    #     segid = c(rep(1:len(ll), ll))
-    #     segdatalist = split(data, segid)
-    #     lengthdata = as.data.table(cbind(length = data[[length]], seg.id = segid))
-    #     seglength.summary = lengthdata[, .(sum(length)), by = .(seg.id)]
-    #     seglength = round(seglength.summary[[2]], digits = 10)
-    #     k = which(seglength > max_length)
-    # }
+    seglength         = seglength_summary.round(10).values
+    k = np.flatnonzero(seglength > max_length)
+
+    while(len(k) > 0):
+        sa = [_seg2(segdatalist[x], var, length, allowed_segment_length_range) + cum_ll[x] for x in k]
+        ss = np.sort(np.concatenate([ss, *sa]))
+
+        k1          = np.array([0, *ss])
+        k2          = np.array([*ss, len(data.index)])
+        ll          = k2 - k1
+        cum_ll      = np.append(np.array([0]), np.cumsum(ll))
+        segid       = np.repeat(np.arange(0, len(ll)), ll)
+        #segdatalist = np.split(data, segid)
+
+        # we are grouping the data wherever _seg2 found a maximum. generally there should be only a single maximum, but if there is an equal maximum at two indices, then there will be 3 groups overall.
+        segdatalist = np.split(data, np.cumsum(ll)[:-1])
+
+        #lengthdata        = pandas.DataFrame([data.loc[:,length], segid], columns = ["length","seg.id"]))
+        seglength_summary = data[length].groupby(segid).sum()
+
+        seglength         = seglength_summary.round(10).values
+        k = np.flatnonzero(seglength > max_length)
+    
     # # add seg.id
-    # k1 = c(1, ss)
-    # k2 = c(ss - 1, nrow(data))
-    # ll = k2 - k1 + 1
-    # data$seg.id = rep(1:len(ll), ll)
-    # data$seg.point = 0
-    # data$seg.point[c(1, ss)] = 1
+    k1                          = np.array([0, *ss])
+    k2                          = np.array([*ss, len(data.index)])
+    ll                          = k2 - k1
+    segid                       = np.repeat(np.arange(0, len(ll)), ll)
+    data["seg.id"]              = segid
+    data["seg.point"]           = 0
+    data.loc[[0,*ss], "seg.point"] = 1
 
     return data
